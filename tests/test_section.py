9eb74e1c6a497c144e6e9034dfd00f22678c8ee1
Bjørn Ådlandsvik <bjorn@imr.no>
2020-08-11 09:32:41 +0200

    Some clean-up

--- tests/test_section.py
+++ tests/test_section.py
@@ -1,64 +1,66 @@
 import numpy as np
 import xarray as xr
-import xgcm
 import xroms
-#import pytest
-
-# Make an idealized test data set
-imax, jmax, kmax = 8, 6, 4
-xx, yy = np.meshgrid(np.arange(imax), np.arange(jmax))
-H = yy + 100
-s_w = np.linspace(-1, 0, kmax + 1)
-s_rho = 0.5 * (s_w[:-1] + s_w[1:])
-z_w = s_w[:, None, None] * H[None, :, :]
-z_rho = s_rho[:, None, None] * H[None, :, :]
-Z = ("s_rho",)
-Zw = ("s_w",)
-YX = ("eta_rho", "xi_rho")
-ZYX = Z + YX
-
-coords = {"z_rho": (Z + YX, z_rho), "z_w": (Zw + YX, z_w)}
-data_vars = {
-    "h": (YX, H),
-    "dx": (YX, np.ones((jmax, imax))),
-    "dy": (YX, np.ones((jmax, imax))),
-    "dz": (Z + YX, z_w[1:, :, :] - z_w[:-1, :, :]),
-    "u": (("s_rho", "eta_rho", "xi_u"), np.multiply.outer(np.ones(kmax), xx[:, :-1])),
-    "v": (("s_rho", "eta_v", "xi_rho"), np.multiply.outer(np.ones(kmax), yy[:-1, :])),
-    "temp": (Z + YX, 20.0 + 0.1 * z_rho),
-}
-
-ds = xr.Dataset(data_vars=data_vars, coords=coords)
-grid_coords = {
-    "X": {"center": "xi_rho", "inner": "xi_u"},
-    "Y": {"center": "eta_rho", "inner": "eta_v"},
-    "Z": {"center": "s_rho", "outer": "s_w"},
-}
-grid = xgcm.Grid(ds, coords=grid_coords)
+import pytest
 
 
-def test_idealized():
+@pytest.fixture
+def ds():
+    """Make an idealized ROMS-like test dataset"""
+    imax, jmax, kmax = 8, 6, 4
+    xx, yy = np.meshgrid(np.arange(imax), np.arange(jmax))
+    H = yy + 100
+    s_w = np.linspace(-1, 0, kmax + 1)
+    s_rho = 0.5 * (s_w[:-1] + s_w[1:])
+    z_w = s_w[:, None, None] * H[None, :, :]
+    z_rho = s_rho[:, None, None] * H[None, :, :]
+
+    # Make xarray Dataset
+    Z = ("s_rho",)
+    Zw = ("s_w",)
+    YX = ("eta_rho", "xi_rho")
+    coords = {"z_rho": (Z + YX, z_rho), "z_w": (Zw + YX, z_w)}
+    data_vars = {
+        "h": (YX, H),
+        "dx": (YX, np.ones((jmax, imax))),
+        "dy": (YX, np.ones((jmax, imax))),
+        "dz": (Z + YX, z_w[1:, :, :] - z_w[:-1, :, :]),
+        "u": (
+            ("s_rho", "eta_rho", "xi_u"),
+            np.multiply.outer(np.ones(kmax), xx[:, :-1]),
+        ),
+        "v": (
+            ("s_rho", "eta_v", "xi_rho"),
+            np.multiply.outer(np.ones(kmax), yy[:-1, :]),
+        ),
+        "temp": (Z + YX, 20.0 + 0.1 * z_rho),
+    }
+
+    return xr.Dataset(data_vars=data_vars, coords=coords)
+
+
+def test_idealized(ds):
     X = np.array([0.8, 1.4, 1.4, 3.0])
     Y = np.array([1.0, 1.0, 1.8, 2.4])
     dss, sgrid = xroms.section(ds, X, Y)
 
     # Geometry
 
-    assert np.allclose(dss.xi_c, [1.1, 1.4, 2.2])
-    assert np.allclose(dss.eta_c, [1.0, 1.4, 2.1])
+    assert np.allclose(dss.xi, [1.1, 1.4, 2.2])
+    assert np.allclose(dss.eta, [1.0, 1.4, 2.1])
     assert np.allclose(dss.ddistance, [0.6, 0.8, np.sqrt(2.92)])
 
     # Velocity
 
     # Manual bilinear interpolation
-    U = ds.u[-1,:,:]
-    V = ds.v[-1,:,:]
-    U0 = 0.4*U[0,0] + 0.6*U[0,1]
-    V0 = 0.45*V[0,1] + 0.45*V[1, 1] + 0.05*V[0,2] + 0.05*V[1,2]
+    U = ds.u[-1, :, :]
+    V = ds.v[-1, :, :]
+    U0 = 0.4 * U[0, 0] + 0.6 * U[0, 1]
+    V0 = 0.45 * V[0, 1] + 0.45 * V[1, 1] + 0.05 * V[0, 2] + 0.05 * V[1, 2]
 
-    ds0 = dss.isel(s_rho=-1)   # Surface values
+    ds0 = dss.isel(s_rho=-1)  # Surface values
     assert abs(ds0.u[0] - U0) < 1e-15
     assert abs(ds0.v[0] - V0) < 1e-15
     assert ds0.u_normal[0] == ds0.v[0]
-    assert ds0.u_normal[1] == - ds0.u[1]
-    assert abs(ds0.u_normal[2] - (-6*ds0.u[2] + 16*ds0.v[2]) / np.sqrt(292)) < 1e-12
+    assert ds0.u_normal[1] == -ds0.u[1]
+    assert abs(ds0.u_normal[2] - (-6 * ds0.u[2] + 16 * ds0.v[2]) / np.sqrt(292)) < 1e-12
--- xroms/section.py
+++ xroms/section.py
@@ -1,19 +1,25 @@
 import numpy as np
 import xarray as xr
 from xgcm import Grid
-#import xroms
+
+# import xroms
+
 
 def section(ds, X, Y):
-    """Create a section Dataset
+    """Create a ROMS section Dataset with xgcm grid
 
-    Arguments:
+    Inputs:
     ds: xroms Dataset
     X:  Seguence of xi_rho coordinates
-    Y: Sequence of eta_rho coordinates
+    Y:  Sequence of eta_rho coordinates
+
+    Outputs:
+    dss:  ROMS section dataset
+    sgrid: xgcm grid object
 
     The X and Y sequence should have the same length and gives the vertices
     (section cell boundaries) of the section. Most variables lives at the
-    center points of the section cells,
+    center points of the section cells.
 
     """
 
@@ -21,14 +27,18 @@ def section(ds, X, Y):
     X_c = xr.DataArray(0.5 * (X[:-1] + X[1:]), dims=["distance"])
     Y_c = xr.DataArray(0.5 * (Y[:-1] + Y[1:]), dims=["distance"])
 
-    # Interpolate U and V to grid centers
-    # U = grid.interp(A.u, 'X')
-    # V = grid.interp(A.v, 'Y')
-
     # Interpolate Dataset to the section
     # offset for xi_u and eta_v to handle the C-grid
     dss = ds.interp(xi_rho=X_c, eta_rho=Y_c, xi_u=X_c - 0.5, eta_v=Y_c - 0.5)
-    dss = dss.drop_vars(['xi_u', 'eta_v'])
+
+    # Remove coordinates and data variables at u, v and psi-points
+    dss = dss.drop_vars(
+        [
+            v
+            for v in list(dss.coords) + list(dss.data_vars)
+            if v.endswith("_u") or v.endswith("_v") or v.endswith("_psi")
+        ]
+    )
 
     # Length of section segments
     dx = dss.dx * np.diff(X)
@@ -36,52 +46,31 @@ def section(ds, X, Y):
     ddistance = np.sqrt(dx ** 2 + dy ** 2)
     dss["ddistance"] = ddistance
 
-    # Normal component of current
+    # Normal and tangential component of current
     dss["u_normal"] = (-dss.u * dy + dss.v * dx) / ddistance
     dss["u_tangent"] = (dss.u * dx + dss.v * dy) / ddistance
-    # T
 
-    # Cumulative distance along section
-    distance = np.concatenate(([0], np.cumsum(ddistance)))  # Unit = m
-    distance_c = 0.5 * (distance[:-1] + distance[1:])
-
-    coords = {
-        "distance": (["distance",], distance_c),
-        "distance_b": (["distance_b",], distance),
-    }
-    dss = dss.assign_coords(coords)
+    # Set cumulative distance along section as coordinates
+    distance_b = np.concatenate(([0], np.cumsum(ddistance)))
+    distance = 0.5 * (distance_b[:-1] + distance_b[1:])
+    dss.coords["distance"] = distance
+    dss.coords["distance_b"] = distance_b
 
     # Add center and boundary positions in grid coordinates
-    dss["xi_c"] = X_c
-    dss["eta_c"] = Y_c
+    dss["xi"] = X_c
+    dss["eta"] = Y_c
     dss["xi_b"] = xr.DataArray(X, dims="distance_b")
     dss["eta_b"] = xr.DataArray(Y, dims="distance_b")
 
-    # Preliminary Grid
-    sgrid0 = Grid(
-        dss,
-        coords={
-            "L": {"center": "distance", "outer": "distance_b"},
-            "Z": {"center": "s_rho", "outer": "s_w"},
-        },
-    )
-
-    #dss["dz"] = sgrid0.diff(dss.z_w, "Z")
+    # Make xgcm grid object
     dss["dArea"] = dss.ddistance * dss.dz
-
-    metrics = {
-        ("L",): ["ddistance"],
-        ("Z",): ["dz"],
-        ("L", "Z"): ["dArea"],
-    }
-
     sgrid = Grid(
         dss,
         coords={
             "L": {"center": "distance", "outer": "distance_b"},
             "Z": {"center": "s_rho", "outer": "s_w"},
         },
-        metrics=metrics,
+        metrics={("L",): ["ddistance"], ("Z",): ["dz"], ("L", "Z"): ["dArea"],},
         periodic=False,
     )
 
